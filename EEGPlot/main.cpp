337132e5f93148249b02e58f48dd5d39d9b3939f
Ciel <50799514+lfsty@users.noreply.github.com>
2023-06-02 10:31:24 +0800

    update (#1)

--- EEGPlot/main.cpp
+++ EEGPlot/main.cpp
@@ -1,13 +1,19 @@
 #include "mainwindow.h"
 
 #include <QApplication>
-
+#include <QSurfaceFormat>
 int main(int argc, char* argv[])
 {
 //#if (QT_VERSION >= QT_VERSION_CHECK(6, 0, 0))
 //    //设置高分屏缩放舍入策略
 //    QApplication::setHighDpiScaleFactorRoundingPolicy(Qt::HighDpiScaleFactorRoundingPolicy::Floor);
 //#endif
+
+    // 垂直同步
+//    QSurfaceFormat format = QSurfaceFormat::defaultFormat();
+//    format.setSwapInterval(0);
+//    QSurfaceFormat::setDefaultFormat(format);
+
 
     QApplication a(argc, argv);
     MainWindow w;
